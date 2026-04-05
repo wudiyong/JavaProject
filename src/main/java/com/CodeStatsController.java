329f2bc6e473e2cdabc733585d1c70510edac994
wudiyong <774695423@qq.com>
2026-04-05 12:40:02 +0800

    modify codeStatsController

--- src/main/java/com/CodeStatsController.java
+++ src/main/java/com/CodeStatsController.java
@@ -1,5 +1,7 @@
 package com;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,9 @@ import java.util.Map;
 public class CodeStatsController {
 
     @PostMapping("/codeStats")
-    public void codeStats(@RequestBody Map<String, Object> params) {
-        System.out.println(params);
-        System.out.println("codeStats");
-        System.out.println("success");
+    public void codeStats(@RequestBody Map<String, Object> params) throws JsonProcessingException {
+        // 把params转换为json字符串
+        String jsonString = new ObjectMapper().writeValueAsString(params);
+        System.out.println(jsonString);
     }
 }
